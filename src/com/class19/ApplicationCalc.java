d7165b91bb5175025bbbe3c8f45b8d85a4ed04d9
Maasai28 <Douglasominde@gmail.com>
2019-04-07 15:13:07 -0400

    Classmkdir Douglas

--- src/com/class19/ApplicationCalc.java
+++ src/com/class19/ApplicationCalc.java
@@ -2,8 +2,11 @@ package com.class19;
 
 public class ApplicationCalc {
 
+	
 	public static void main(String[] args) {
 		Calculator.average(10, 20, 30);
+		Calculator.sum(10, 20, 40);
+		
 			
 		}
 	}
